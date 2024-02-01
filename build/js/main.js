dc784652559d0b124a6f203482dec6ee4b3c3196
Pranay <pranay.das0909@gmail.com>
2024-02-01 13:27:57 +0530

    Fix text toggle

--- build/js/main.js
+++ build/js/main.js
@@ -2,14 +2,25 @@ const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
 
+    const shortText = document.getElementById('short-text')
+    const toggleButton = document.getElementById('toggle-read')
+
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
         hamburgerBtn.classList.toggle('toggle-btn')
     }
 
+    const toggleTextBlock = () => {
+        shortText.classList.toggle('hidden')
+        // toggleButton.classList.toggle("visible");
+        toggleButton.textContent = shortText.classList.contains("hidden") ? "Read More" : "Read Less";
+    }
+
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
+
+    toggleButton.addEventListener('click', toggleTextBlock)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
